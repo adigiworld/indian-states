1fe210febf3623d05d3f96f81c2d4f75eff09d5b
adigiworld <asadigiworld@gmail.com>
2021-10-31 00:37:29 +0530

    Added index.js lib file

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// import { readFile } from 'fs/promises';
+// const data = JSON.parse(await readFile(new URL('./IndianStatesUTCapitals.json', import.meta.url), 'UTF-8'))
+
+// const allStates = data.States;
+// const allUT = data.UT;
+
+// export const getAllStates = () => {
+//   const states = []
+//   for (const state in allStates) {
+//     if (Object.hasOwnProperty.call(allStates, state)) {
+//       states.push(state);
+//     }
+//   }
+//   return states;
+// }
+
+
+const data = require('./IndianStatesUTCapitals.json');
+const allStates = data.States;
+const allUT = data.UT;
+
+const getAllStates = () => {
+  const states = []
+  for (let state in allStates) {
+    if (Object.hasOwnProperty.call(allStates, state)) {
+      states.push(state);
+    }
+  }
+  return states;
+}
+
+const getAllUT = () => {
+  const uts = []
+  for (let ut in allUT) {
+    if (Object.hasOwnProperty.call(allUT, ut)) {
+      uts.push(ut);
+    }
+  }
+  return uts;
+}
+
+module.exports = { getAllStates, getAllUT };

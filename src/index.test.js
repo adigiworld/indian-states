a351f9e613c7bf23edc15d2ec5ef63a655c3f71c
adigiworld <asadigiworld@gmail.com>
2021-10-31 14:36:02 +0530

    Writed test

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const { describe, it } = require('mocha');
+const { getAllStates, getAllUT } = require('./index');
 
 describe('Indian States', () => {
 
@@ -7,4 +8,18 @@ describe('Indian States', () => {
     expect(true).to.be.true;
   });
 
+  it('Should have a list of all States', () => {
+    expect(isArrayOfStrings(getAllStates())).to.be.true;
+  });
+
+  it('Should have a list of all UTs', () => {
+    expect(isArrayOfStrings(getAllUT())).to.be.true;
+  });
+
 });
+
+function isArrayOfStrings(array) {
+  return array.every(item => {
+    return typeof item === 'string';
+  });
+}

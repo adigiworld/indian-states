e4fe8f0875ad378792543ce022c1a14ff1bdea8e
adigiworld <asadigiworld@gmail.com>
2021-11-01 11:06:06 +0530

    Fixed coverage

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // import { readFile } from 'fs/promises';
-// const data = JSON.parse(await readFile(new URL('./IndianStatesUTCapitals.json', import.meta.url), 'UTF-8'))
+// const data = JSON.parse(readFile(new URL('./IndianStatesUTCapitals.json', import.meta.url), 'UTF-8'));
 
 // const allStates = data.States;
 // const allUT = data.UT;
 
 // export const getAllStates = () => {
-//   const states = []
+//   const states = [];
 //   for (const state in allStates) {
 //     if (Object.hasOwnProperty.call(allStates, state)) {
 //       states.push(state);
 //     }
 //   }
 //   return states;
-// }
+// };
 
 const data = require('./IndianStatesUTCapitals.json');
 const allStates = data.States;
@@ -21,9 +21,7 @@ const allUT = data.UT;
 const getAllStates = () => {
   const states = [];
   for (let state in allStates) {
-    if (Object.hasOwnProperty.call(allStates, state)) {
-      states.push(state);
-    }
+    states.push(state);
   }
   return states;
 };
@@ -31,9 +29,7 @@ const getAllStates = () => {
 const getAllUT = () => {
   const uts = [];
   for (let ut in allUT) {
-    if (Object.hasOwnProperty.call(allUT, ut)) {
-      uts.push(ut);
-    }
+    uts.push(ut);
   }
   return uts;
 };
